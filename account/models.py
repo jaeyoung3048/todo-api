8fd950cf27d8ba33993303acb7f32082d76d44aa
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-06 18:21:19 +0900

    UPDATE: utils.jwt delete and settings update

--- account/models.py
+++ account/models.py
@@ -1,2 +1,50 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
+from account.manager import UserManager
+
+import uuid
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    object = UserManager()
+
+    uid = models.UUIDField(
+        unique=True,
+        editable=False,
+        default=uuid.uuid4,
+        verbose_name='Public identifier'
+    )
+
+    email = models.EmailField(
+        unique=True,
+        max_length=128,
+    )
+
+    username = models.CharField(
+        unique=True,
+        max_length=128,
+    )
+
+    first_name = models.CharField(
+        blank=False,
+        max_length=128
+    )
+
+    last_name = models.CharField(
+        blank=False,
+        max_length=128
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    is_admin = models.BooleanField(
+        default=False
+    )
+
+    EMAIL_FIELDS = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'username']
--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'account',
 ]
+
+REST_FRAMEWORK = {
+
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -98,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'account.User'
 
 
 # Internationalization
