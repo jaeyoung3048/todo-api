096b7d77ba86a0ac7c15663180ed2e86523763e0
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-10 14:38:40 +0900

    FIX: LANGUAGE_CODE en-us -> ko-kr

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     'drf_yasg',
 
     'account',
@@ -149,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
