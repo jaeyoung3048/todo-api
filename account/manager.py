a4d101d55f852c60ef682d2768742e97369c90e6
jaeyoung <56381650+jaeyoung3048@users.noreply.github.com>
2021-03-30 13:38:34 +0900

    UPDATE: model manager, url

--- account/manager.py
+++ account/manager.py
@@ -15,15 +15,15 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_admin', False)
+        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
         return self._create_user(email, password, **extra_fields)
 
     def create_staff(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_admin', False)
+        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('auth/token', account_views.SigninView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup', account_views.SignupView.as_view(), name='sign_up'),
-    path('sms<int:phonenumber>', account_views.sms_authentication_view, name='sms'),
+    path('sms?<int:phonenumber>', account_views.sms_authentication_view, name='sms'),
     path('state', account_views.account_active_state_view)
 ]
